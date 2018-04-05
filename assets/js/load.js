a2da90350d316378479895d3c54dddb526fd2fa1
Izæt <izet0004@elev.techcollege.dk>
2018-04-05 12:16:04 +0200

    first

---
+++ assets/js/load.js
@@ -0,0 +1,27 @@
+// document.addEventListener('DOMContentLoaded', function() { 
+//     var content = Array.from(document.getElementsByClassName("noload"));
+//     var loading = document.getElementById("load")
+//     // window.setTimeout()
+//     content.forEach(element => {
+//         element.classList.remove("noload");
+
+//     });
+//     console.log(loading);
+//     loading.classList.add("noload");
+//  });
+
+// window.onload = function () {
+//     var content = Array.from(document.getElementsByClassName("noload"));
+//     content.forEach(element => {
+//         element.classList.remove("noload");
+//     });
+// }
+
+// $(document).load(function () {
+//     var content = Array.from(document.getElementsByClassName("noload"));
+//     var load = document.getElementById("load");
+//     load.classList.add("noload");
+//     content.forEach(element => {
+//         element.classList.remove("noload");
+//     });
+// })
